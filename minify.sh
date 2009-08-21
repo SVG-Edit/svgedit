623fa4287fa6649f622dc0b5fe0be76e4d1263ef
Pavol Rusnak <stick@gk2.sk>
2009-08-21 13:44:54 +0000

    dir structure cleanup
    git-svn-id: http://svg-edit.googlecode.com/svn/trunk@431 eee81c28-f429-11dd-99c0-75d572ba1ddd

--- minify.sh
+++ minify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-YUI=yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar
+YUI=yuicompressor-*.jar
 
 # minify spin button
 java -jar $YUI editor/spinbtn/JQuerySpinBtn.js > editor/spinbtn/JQuerySpinBtn.min.js
@@ -7,7 +7,6 @@ java -jar $YUI editor/spinbtn/JQuerySpinBtn.js > editor/spinbtn/JQuerySpinBtn.mi
 # minify SVG-edit files
 java -jar $YUI editor/svg-editor.js > editor/svg-editor.min.js
 java -jar $YUI editor/svgcanvas.js > editor/svgcanvas.min.js
-
 
 # CSS files do not work remotely
 # java -jar $YUI editor/spinbtn/JQuerySpinBtn.css > editor/spinbtn/JQuerySpinBtn.min.css
