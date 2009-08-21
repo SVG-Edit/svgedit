2eea448e9a5581c4bf0f72b33af60244d8524710
Pavol Rusnak <stick@gk2.sk>
2009-08-21 14:03:39 +0000

    replaced scripts with makefile (call make all to build)
    git-svn-id: http://svg-edit.googlecode.com/svn/trunk@432 eee81c28-f429-11dd-99c0-75d572ba1ddd

--- firefox-extension/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DST="content/editor"
-if [ -e "${DST}" ]; then
-    rm -rf "${DST}"
-fi
-cp -R ../editor content/
-SVNS=`find content/editor -name '.svn'`
-rm -rf ${SVNS}
--- firefox-extension/mk_xpi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-./build.sh
-zip -r ../svg-edit.xpi *
