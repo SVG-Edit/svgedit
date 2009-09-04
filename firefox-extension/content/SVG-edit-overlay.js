98d0f897390d83967e6c47b6efd729bc94f80955
rusnakp <rusnakp@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2009-09-04 16:10:50 +0000

    create one Makefile in the top root directory (WIP - need to remove build path from resulting archives)

--- firefox-extension/content/SVG-edit-overlay.js
+++
@@ -1,6 +0,0 @@
-function start_svg_edit() {
-    var url = "chrome://SVG-edit/content/editor/svg-editor.html";
-
-    window.openDialog(url, "SVG Editor",
-		      "width=1024,height=700,menubar=no,toolbar=no");
-}
--- firefox-extension/handlers.js
+++ firefox-extension/handlers.js
@@ -1,7 +1,7 @@
 // Note: This JavaScript file must be included as the last script on the main HTML editor page to override the open/save handlers
 $(function() {
 	if(!window.Components) return;
-	
+
 	function moz_file_picker(readflag) {
 	    var fp = window.Components.classes["@mozilla.org/filepicker;1"].
 		createInstance(Components.interfaces.nsIFilePicker);
@@ -13,7 +13,7 @@ $(function() {
 	    fp.show();
 	    return fp.file;
 	}
-	
+
 	svgCanvas.setCustomHandlers({
 		'open':function() {
 		    try {
@@ -22,12 +22,12 @@ $(function() {
 			var file = moz_file_picker(true);
 			if(!file)
 			    return(null);
-			
-                        var inputStream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);                      
-                        inputStream.init(file, 0x01, 00004, null);
-                        var sInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);                   
-                        sInputStream.init(inputStream);
-                        svgCanvas.setSvgString(sInputStream.
+
+			var inputStream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream);
+			inputStream.init(file, 0x01, 00004, null);
+			var sInputStream = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance(Components.interfaces.nsIScriptableInputStream);
+			sInputStream.init(inputStream);
+			svgCanvas.setSvgString(sInputStream.
 					    read(sInputStream.available()));
 		    } catch(e) {
                         console.log("Exception while attempting to load" + e);
@@ -38,10 +38,10 @@ $(function() {
 				var file = moz_file_picker(false);
 				if(!file)
 					return;
-				
+
 							if (!file.exists())
 					file.create(0, 0664);
-				
+
 				var out = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
 				out.init(file, 0x20 | 0x02, 00004,null);
 				out.write(str, str.length);
