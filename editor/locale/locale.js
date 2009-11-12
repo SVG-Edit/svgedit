6d30a6b7ee403f5e6d31e4a44eb166b0e6d5e990
Alexis Deveria <adeveria@gmail.com>
2009-11-12 21:28:04 +0000

    Possibly fixed lang detection script
    git-svn-id: http://svg-edit.googlecode.com/svn/trunk@939 eee81c28-f429-11dd-99c0-75d572ba1ddd

--- editor/locale/locale.js
+++ editor/locale/locale.js
@@ -12,12 +12,8 @@ var put_locale = function(svgCanvas, given_param){
 			if (lang_param == "")
 				return;
 		}
-		if(typeof(lang_param) != 'string') {
-			alert(typeof(lang_param));
-			alert(lang_param)
-		}
 		// don't bother on first run if language is English
-		if(lang_param.indexOf("en") == 0) return;
+		if(String(lang_param).indexOf("en") == 0) return;
 	}
 	
 	var url = "locale/lang." + lang_param + ".js";
