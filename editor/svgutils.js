593beb421581092a1bb5480277a9be6d595e1dcb
Jeff Schiller <codedread@gmail.com>
2010-10-26 17:11:23 +0000

    Move a few more functions into svgutils.js
    git-svn-id: http://svg-edit.googlecode.com/svn/trunk@1822 eee81c28-f429-11dd-99c0-75d572ba1ddd

--- editor/svgutils.js
+++ editor/svgutils.js
@@ -257,4 +257,66 @@ svgedit.Utilities.bboxToObj = function(bbox) {
 	}
 };
 
+// Function: walkTree
+// Walks the tree and executes the callback on each element in a top-down fashion
+//
+// Parameters:
+// elem - DOM element to traverse
+// cbFn - Callback function to run on each element
+svgedit.Utilities.walkTree = function(elem, cbFn){
+	if (elem && elem.nodeType == 1) {
+		cbFn(elem);
+		var i = elem.childNodes.length;
+		while (i--) {
+			svgedit.Utilities.walkTree(elem.childNodes.item(i), cbFn);
+		}
+	}
+};
+
+// Function: walkTreePost
+// Walks the tree and executes the callback on each element in a depth-first fashion
+// TODO: FIXME: Shouldn't this be calling walkTreePost?
+//
+// Parameters:
+// elem - DOM element to traverse
+// cbFn - Callback function to run on each element
+svgedit.Utilities.walkTreePost = function(elem, cbFn) {
+	if (elem && elem.nodeType == 1) {
+		var i = elem.childNodes.length;
+		while (i--) {
+			svgedit.Utilities.walkTree(elem.childNodes.item(i), cbFn);
+		}
+		cbFn(elem);
+	}
+};
+
+// Function: getUrlFromAttr
+// Extracts the URL from the url(...) syntax of some attributes.  
+// Three variants:
+// 	* <circle fill="url(someFile.svg#foo)" />
+//  * <circle fill="url('someFile.svg#foo')" />
+//  * <circle fill='url("someFile.svg#foo")' />
+//
+// Parameters:
+// attrVal - The attribute value as a string
+// 
+// Returns:
+// String with just the URL, like someFile.svg#foo
+svgedit.Utilities.getUrlFromAttr = function(attrVal) {
+	if (attrVal) {		
+		// url("#somegrad")
+		if (attrVal.indexOf('url("') === 0) {
+			return attrVal.substring(5,attrVal.indexOf('"',6));
+		}
+		// url('#somegrad')
+		else if (attrVal.indexOf("url('") === 0) {
+			return attrVal.substring(5,attrVal.indexOf("'",6));
+		}
+		else if (attrVal.indexOf("url(") === 0) {
+			return attrVal.substring(4,attrVal.indexOf(')'));
+		}
+	}
+	return null;
+};
+
 })();
