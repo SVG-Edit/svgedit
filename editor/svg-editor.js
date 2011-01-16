3ffc78a81cc2d4b15fb3c2066a94f9644e645bb3
Jeff Schiller <codedread@gmail.com>
2011-01-16 16:30:29 +0000

    Remove debugging statements
    git-svn-id: http://svg-edit.googlecode.com/svn/trunk@1920 eee81c28-f429-11dd-99c0-75d572ba1ddd

--- editor/svg-editor.js
+++ editor/svg-editor.js
@@ -3536,7 +3536,6 @@
 			$('#layer_new').click(function() {
 				var i = svgCanvas.getNumLayers();
 				do {
-					debugger;
 					var uniqName = uiStrings.layers.layer + " " + ++i;
 				} while(svgCanvas.hasLayer(uniqName));
 				
