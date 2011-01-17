d0ba1a9e5f63e5a105e73c37ec1d160c694523a5
codedread <codedread@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2011-01-17 04:36:54 +0000

    Add identifyLayers() to svg.draw.Drawing with tests

--- editor/draw.js
+++ editor/draw.js
@@ -8,6 +8,8 @@
 
 // Dependencies:
 // 1) jQuery
+// 2) browser.js
+// 3) svgutils.js
 
 (function() {
 if (!window.svgedit) {
@@ -179,6 +181,70 @@ svgedit.draw.Drawing.prototype.hasLayer = function(name) {
 		if(this.all_layers[i][0] == name) return true;
 	}
 	return false;
+};
+
+// Function: svgedit.draw.Drawing.identifyLayers
+// Updates layer system
+svgedit.draw.Drawing.prototype.identifyLayers = function() {
+	this.all_layers = [];
+	var numchildren = this.svgElem_.childNodes.length;
+	// loop through all children of SVG element
+	var orphans = [], layernames = [];
+	var current_layer = null;
+	var childgroups = false;
+	for (var i = 0; i < numchildren; ++i) {
+		var child = this.svgElem_.childNodes.item(i);
+		// for each g, find its layer name
+		if (child && child.nodeType == 1) {
+			if (child.tagName == "g") {
+				childgroups = true;
+				var name = $("title",child).text();
+				
+				// Hack for Opera 10.60
+				if(!name && svgedit.browser.isOpera() && child.querySelectorAll) {
+					name = $(child.querySelectorAll('title')).text();
+				}
+
+				// store layer and name in global variable
+				if (name) {
+					layernames.push(name);
+					this.all_layers.push( [name,child] );
+					current_layer = child;
+					svgedit.utilities.walkTree(child, function(e){e.setAttribute("style", "pointer-events:inherit");});
+					current_layer.setAttribute("style", "pointer-events:none");
+				}
+				// if group did not have a name, it is an orphan
+				else {
+					orphans.push(child);
+				}
+			}
+			// if child has a bbox (i.e. not a <title> or <defs> element), then it is an orphan
+			else if(svgedit.utilities.getBBox(child) && child.nodeName != 'defs') { // Opera returns a BBox for defs
+				var bb = svgedit.utilities.getBBox(child);
+				orphans.push(child);
+			}
+		}
+	}
+	
+	// create a new layer and add all the orphans to it
+	var svgdoc = this.svgElem_.ownerDocument;
+	if (orphans.length > 0 || !childgroups) {
+		var i = 1;
+		// TODO(codedread): What about internationalization of "Layer"?
+		while (layernames.indexOf(("Layer " + i)) >= 0) { i++; }
+		var newname = "Layer " + i;
+		current_layer = svgdoc.createElementNS(svg_ns, "g");
+		var layer_title = svgdoc.createElementNS(svg_ns, "title");
+		layer_title.textContent = newname;
+		current_layer.appendChild(layer_title);
+		for (var j = 0; j < orphans.length; ++j) {
+			current_layer.appendChild(orphans[j]);
+		}
+		this.svgElem_.appendChild(current_layer);
+		this.all_layers.push( [newname, current_layer] );
+	}
+	svgedit.utilities.walkTree(current_layer, function(e){e.setAttribute("style","pointer-events:inherit");});
+	current_layer.setAttribute("style","pointer-events:all");
 };
 
 
