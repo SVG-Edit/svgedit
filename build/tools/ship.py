c6cd4c800aec3447520142da89fda23935b00a01
Jeff Schiller <codedread@gmail.com>
2011-01-18 19:04:34 +0000

    Fix inclusion of jQuery in release-mode.  Tweak shippy
    git-svn-id: http://svg-edit.googlecode.com/svn/trunk@1934 eee81c28-f429-11dd-99c0-75d572ba1ddd

--- build/tools/ship.py
+++ build/tools/ship.py
@@ -6,17 +6,16 @@
 # Licensed under the Apache 2 License as is the rest of the project
 # Copyright (c) 2011 Jeff Schiller
 #
-# This script has very little real-world application.  It is only used in our pure-client web 
-# app served on GoogleCode so we can have one HTML file, run a build script and generate a 'release'
-# version without having to maintain two separate HTML files.
+# This script has very little real-world application.  It is only used in our pure-client web app
+# served on GoogleCode so we can have one HTML file, run a build script and generate a 'release'
+# version without having to maintain two separate HTML files.  It does this by evaluating
+# 'processing comments' that are suspicously similar to IE conditional comments and then outputting
+# a new HTML file after evaluating particular variables.
 #
 # This script takes the following inputs:
 #
 # * a HTML file (--i=in.html)
 # * a series of flag names (--on=Foo --on=Bar)
-#
-# It parses, the HTML file, enables/disables sections of the markup based
-# on if-else comments and flag values, then outputs a new HTML file.
 #
 # Example:
 #
@@ -125,12 +124,9 @@ def parseComment(line, line_num, enabled_flags):
 
 def ship(inFileName, enabled_flags):
   # read in HTML file
-  in_file = file(inFileName, 'r')
-
-  i = 0
-  lines = in_file.readlines()
-
+  lines = file(inFileName, 'r').readlines()
   out_lines = []
+  i = 0
 
   # loop for each line of markup
   for line in lines:
