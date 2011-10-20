e0daaeaebc3f247d35a8e3b015793a1d2032269d
rusnakp@gmail.com <rusnakp@gmail.com@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2011-10-20 15:27:36 +0000

    add google verification remove trunk (what's that doing here?)

--- trunk/extras/server-save/svg-editor-save.js
+++
@@ -1,3 +0,0 @@
-this.saveHandler = function(svg) {
-	$.post("svg-editor-save.php", { svg_data: escape(svg) } );
-});
--- trunk/firefox-extension/content/svg-edit-overlay.js
+++
@@ -1,4 +0,0 @@
-function start_svg_edit() {
-    var url = "chrome://svg-edit/content/editor/svg-editor.html";
-    window.openDialog(url, "SVG Editor", "width=1024,height=700,menubar=no,toolbar=no");
-}
