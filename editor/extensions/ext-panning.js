21bdd45b95eca08540a0677f2a5ca4626b52987f
delapouite@gmail.com <delapouite@gmail.com@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2013-02-16 11:21:06 +0000

    fixed formatting in panning extension

--- editor/extensions/ext-panning.js
+++ editor/extensions/ext-panning.js
@@ -10,39 +10,35 @@
 /* 
 	This is a very basic SVG-Edit extension to let tablet/mobile devices panning without problem
 */
- 
-svgEditor.addExtension("ext-panning", function() {
 
-		return {
-			name: "Extension Panning",
-			svgicons: "extensions/ext-panning.xml",
-			
-			buttons: [{
-				id: "ext-panning", 
-				type: "mode", 
-				title: "Panning", 
-				events: {
-					'click': function() {
-						svgCanvas.setMode("ext-panning");
-					}
-				}
-			}],
-			mouseDown: function() {
-				if(svgCanvas.getMode() == "ext-panning") {
-			                svgEditor.setPanning(true);	
-					return {started: true};
-				}
-			},
-			
-			mouseUp: function(opts) {
-				if(svgCanvas.getMode() == "ext-panning") {
-					svgEditor.setPanning(false);
-					return {
-						keep: false,
-						element: null
-					}
+svgEditor.addExtension('ext-panning', function() {
+	return {
+		name: 'Extension Panning',
+		svgicons: 'extensions/ext-panning.xml',
+		buttons: [{
+			id: 'ext-panning',
+			type: 'mode',
+			title: 'Panning',
+			events: {
+				click: function() {
+					svgCanvas.setMode('ext-panning');
 				}
 			}
-		};
+		}],
+		mouseDown: function() {
+			if(svgCanvas.getMode() == 'ext-panning') {
+				svgEditor.setPanning(true);
+				return {started: true};
+			}
+		},
+		mouseUp: function() {
+			if(svgCanvas.getMode() == 'ext-panning') {
+				svgEditor.setPanning(false);
+				return {
+					keep: false,
+					element: null
+				};
+			}
+		}
+	};
 });
-
