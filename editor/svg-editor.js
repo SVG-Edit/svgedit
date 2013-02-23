32564a90ec199154ea08cf7bec2ddcfc0ca1f2d7
delapouite@gmail.com <delapouite@gmail.com@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2013-02-23 18:09:16 +0000

    removed non properly deleted test config introduced in r1467

--- editor/svg-editor.js
+++ editor/svg-editor.js
@@ -4731,21 +4731,3 @@
 	$(svgEditor.init);
 
 })();
-
-// ?iconsize=s&bkgd_color=555
-
-// svgEditor.setConfig({
-// // 	imgPath: 'foo',
-// 	dimensions: [800, 600],
-// 	canvas_expansion: 5,
-// 	initStroke: {
-// 		color: '0000FF',
-// 		width: 3.5,
-// 		opacity: .5
-// 	},
-// 	initFill: {
-// 		color: '550000',
-// 		opacity: .75
-// 	},
-// 	extensions: ['ext-helloworld.js']
-// })
