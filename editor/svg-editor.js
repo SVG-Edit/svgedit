077bf5b66ac1eb79977d53b6b5f1dedf9ec9cf10
delapouite@gmail.com <delapouite@gmail.com@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2013-02-23 17:37:05 +0000

    removed unneeded json_encode / json_decode for 3 reasons: - it's available natively since IE8 and it's provided by Chrome Frame which is a requirement for old IE - it's only used for postMessage exchange which is a newer API than JSON - if ever we would need it, it should be added in an external lib, not inside the editor code itself

--- editor/svg-editor.js
+++ editor/svg-editor.js
@@ -4588,38 +4588,10 @@
 
 			// Callback handler for embedapi.js
 			try {
-				var json_encode = function(obj){
-					//simple partial JSON encoder implementation
-					if (window.JSON && JSON.stringify) return JSON.stringify(obj);
-					var enc = arguments.callee; //for purposes of recursion
-					if (typeof obj == 'boolean' || typeof obj == 'number'){
-						return obj+''; //should work...
-					} else if (typeof obj == 'string') {
-						//a large portion of this is stolen from Douglas Crockford's json2.js
-						return '"'+
-							obj.replace(
-								/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g
-								, function (a) {
-									return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
-							})
-							+'"'; //note that this isn't quite as purtyful as the usualness
-					} else if (obj.length) { //simple hackish test for arrayish-ness
-						for (var i = 0; i < obj.length; i++){
-							obj[i] = enc(obj[i]); //encode every sub-thingy on top
-						}
-						return '['+obj.join(',')+']';
-					} else {
-						var pairs = []; //pairs will be stored here
-						for (var k in obj){ //loop through thingys
-							pairs.push(enc(k)+':'+enc(obj[k])); //key: value
-						}
-						return '{'+pairs.join(',')+'}'; //wrap in the braces
-					}
-				};
 				window.addEventListener('message', function(e) {
 					var cbid = parseInt(e.data.substr(0, e.data.indexOf(';')), 10);
 					try {
-						e.source.postMessage('SVGe'+cbid+';'+json_encode(eval(e.data)), '*');
+						e.source.postMessage('SVGe'+cbid+';'+JSON.stringify(eval(e.data)), '*');
 					} catch(err) {
 						e.source.postMessage('SVGe'+cbid+';error:'+err.message, '*');
 					}
