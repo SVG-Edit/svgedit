97a3d463175762199dd0fc46526074b1fa5f42da
waldir.pimenta@gmail.com <waldir.pimenta@gmail.com@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2013-10-03 11:40:10 +0000

    prettify outputted html

--- 2.6/test/roundtrip-test-src/launcher.php
+++ 2.6/test/roundtrip-test-src/launcher.php
@@ -45,42 +45,44 @@ $revs = get_revs($mysqli);
 <body>
 	<h2>Donate this browser to running tests:</h2>
 	<form action="test-runner.php" method="GET">
-	Revision to test: <select size="1" name="rev">
-	<?php 
+	  Revision to test: <select size="1" name="rev">
+ <?php 
 	foreach($revs as $rev)
 	{
 		$r = $rev["r"];
 		$date = $rev["date"];
-		echo ("<option value=\"$r\">$r - $date</option>");
+		echo ("      <option value=\"$r\">$r - $date</option>\n");
 	}
-	?>
-	</select>
-	<input type="submit" value="Go!" />
+ ?>
+	  </select>
+	  <input type="submit" value="Go!" />
 	</form>
 	<h2>Results:</h2>
-	<p><strong>Raster Error Score summary</strong>
-	<table>
-	<tr><th rowspan="2">Revision</th><th colspan="2">Browser</th><th rowspan="2">Error Score</th></tr>
-	<tr><th>Name</th><th>Version</th></tr>
-	<?php 
+	<p>
+    <strong>Raster Error Score summary</strong>
+	  <table>
+	    <tr><th rowspan="2">Revision</th><th colspan="2">Browser</th><th rowspan="2">Error Score</th></tr>
+	    <tr><th>Name</th><th>Version</th></tr>
+ <?php 
 	while($resultArray = $result->fetch_assoc())
 	{
-		echo ("<tr><td>" . $resultArray["svnRev"] . "</td><td>" . $resultArray["browser"] . "</td><td>" . $resultArray["browserMajorVer"] . "</td><td>");
+		echo ("      <tr><td>" . $resultArray["svnRev"] . "</td><td>" . $resultArray["browser"] . "</td><td>" . $resultArray["browserMajorVer"] . "</td><td>");
 		if($resultArray["count"] == 300){
 			echo $resultArray["sum"];
 		} else {
 			echo "(tests incomplete)";
 		}
-		echo "</td></tr>";
-		
-	}?>
-	</table>
+		echo "</td></tr>\n";
+	}
+?>
+	  </table>
 	</p>
-	<p><strong>Detail views</strong>
-	<ul>
-	<li><a target="_blank" href="rasterview.php">Raster image comparisons</a></li>
-	<li><a target="_blank" href="diffview.php">Canonical XML side-by-side diffs</a></li>
-	</ul>
+	<p>
+    <strong>Detail views</strong>
+	  <ul>
+	    <li><a target="_blank" href="rasterview.php">Raster image comparisons</a></li>
+	    <li><a target="_blank" href="diffview.php">Canonical XML side-by-side diffs</a></li>
+	  </ul>
 	</p>
 </body>
 </html>
