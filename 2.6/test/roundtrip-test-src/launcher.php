5e0964462793ddcef9e1250d6acfadb6ef39ad3f
waldir.pimenta@gmail.com <waldir.pimenta@gmail.com@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2013-10-04 12:26:50 +0000

    re-add borders to cells. valid html5 (p element can't contain block elements)

--- 2.6/test/roundtrip-test-src/launcher.php
+++ 2.6/test/roundtrip-test-src/launcher.php
@@ -28,24 +28,27 @@ $result = $mysqli->query($sql);
 checkForNewRevisions($mysqli);
 $revs = get_revs($mysqli);
 ?>
-<html>
+<!doctype html>
+<html lang="en">
 <head>
 	<title>svg-edit round-trip test launcher</title>
+	<meta charset="utf-8">
 	<style>
 	table {
-		border: 1px solid black;
 		border-collapse: collapse;
 		margin: 1em 0;
 	}
 	td, th {
 		padding: 0 0.5em;
+		border: 1px solid silver;
 	}
 	</style>
 </head>
 <body>
 	<h2>Donate this browser to running tests:</h2>
 	<form action="test-runner.php" method="GET">
-		Revision to test: <select size="1" name="rev">
+		Revision to test:
+		<select size="1" name="rev">
 <?php 
 	foreach($revs as $rev)
 	{
@@ -58,15 +61,14 @@ $revs = get_revs($mysqli);
 		<input type="submit" value="Go!" />
 	</form>
 	<h2>Results:</h2>
-	<p>
-		<strong>Raster Error Score summary</strong>
-		<table>
-			<tr><th rowspan="2">Revision</th><th colspan="2">Browser</th><th rowspan="2">Error Score</th></tr>
-			<tr><th>Name</th><th>Version</th></tr>
+	<strong>Raster Error Score summary</strong>
+	<table>
+		<tr><th rowspan="2">Revision</th><th colspan="2">Browser</th><th rowspan="2">Error Score</th></tr>
+		<tr><th>Name</th><th>Version</th></tr>
 <?php 
 	while($resultArray = $result->fetch_assoc())
 	{
-		echo ("\t\t\t<tr>" .
+		echo ("\t\t<tr>" .
 		      "<td><a href='https://code.google.com/p/svg-edit/source/detail?r=" . $resultArray["svnRev"] . "'>" . $resultArray["svnRev"] . "</a></td>" .
 		      "<td>" . $resultArray["browser"] . "</td>" .
 		      "<td>" . $resultArray["browserMajorVer"] . "</td>" .
@@ -79,14 +81,11 @@ $revs = get_revs($mysqli);
 		echo "</td></tr>\n";
 	}
 ?>
-		</table>
-	</p>
-	<p>
-		<strong>Detail views</strong>
-		<ul>
-			<li><a target="_blank" href="rasterview.php">Raster image comparisons</a></li>
-			<li><a target="_blank" href="diffview.php">Canonical XML side-by-side diffs</a></li>
-		</ul>
-	</p>
+	</table>
+	<strong>Detail views</strong>
+	<ul>
+		<li><a target="_blank" href="rasterview.php">Raster image comparisons</a></li>
+		<li><a target="_blank" href="diffview.php">Canonical XML side-by-side diffs</a></li>
+	</ul>
 </body>
 </html>
