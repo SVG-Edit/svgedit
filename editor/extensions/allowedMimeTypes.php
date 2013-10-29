96bec02730d24d8d4719c60203201c2e0b2fa955
brettz9 <brettz9@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2013-10-29 07:26:29 +0000

    Filter out bad MIME types from fileopen.php and share allowable array with filesave.php

---
+++ editor/extensions/allowedMimeTypes.php
@@ -0,0 +1,11 @@
+<?php
+
+$allowedMimeTypesBySuffix = array(
+    'svg' => 'image/svg+xml',
+    'png' => 'image/png',
+    'jpeg' => 'image/jpeg',
+    'bmp' => 'image/bmp',
+    'webp' => 'image/webp'
+);
+
+?>
