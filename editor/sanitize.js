746c8406ec07ae45e3452450598e4c3bc5f15871
brettz9 <brettz9@eee81c28-f429-11dd-99c0-75d572ba1ddd>
2014-01-31 01:05:03 +0000

    More JSLint

--- editor/sanitize.js
+++ editor/sanitize.js
@@ -1,3 +1,5 @@
+/*globals $, svgedit*/
+/*jslint vars: true, eqeq: true*/
 /**
  * Package: svgedit.sanitize
  *
@@ -12,7 +14,7 @@
 // 2) browser.js
 // 3) svgutils.js
 
-(function() {
+(function() {'use strict';
 
 if (!svgedit.sanitize) {
   svgedit.sanitize = {};
@@ -138,12 +140,12 @@ svgedit.sanitize.sanitizeSvg = function(node) {
 
   var allowedAttrs = svgWhiteList_[node.nodeName];
   var allowedAttrsNS = svgWhiteListNS_[node.nodeName];
-
+  var i;
   // if this element is supported, sanitize it
   if (typeof allowedAttrs !== 'undefined') {
 
     var seAttrs = [];
-    var i = node.attributes.length;
+    i = node.attributes.length;
     while (i--) {
       // if the attribute is not in our whitelist, then remove it
       // could use jQuery's inArray(), but I don't know if that's any better
@@ -173,6 +175,7 @@ svgedit.sanitize.sanitizeSvg = function(node) {
         case 'patternTransform':
           var val = attr.nodeValue.replace(/(\d)-/g, '$1 -');
           node.setAttribute(attrName, val);
+          break;
         }
       }
 
@@ -202,8 +205,7 @@ svgedit.sanitize.sanitizeSvg = function(node) {
     var href = svgedit.utilities.getHref(node);
     if (href &&
       ['filter', 'linearGradient', 'pattern',
-      'radialGradient', 'textPath', 'use'].indexOf(node.nodeName) >= 0)
-    {
+      'radialGradient', 'textPath', 'use'].indexOf(node.nodeName) >= 0) {
       // TODO: we simply check if the first character is a #, is this bullet-proof?
       if (href[0] != '#') {
         // remove the attribute (but keep the element)
@@ -248,9 +250,9 @@ svgedit.sanitize.sanitizeSvg = function(node) {
     parent.removeChild(node);
 
     // call sanitizeSvg on each of those children
-    var i = children.length;
+    i = children.length;
     while (i--) { svgedit.sanitize.sanitizeSvg(children[i]); }
   }
 };
 
-})();
+}());
