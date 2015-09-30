f1b67e415905d0fd7b4fb9364507a5be8396f2b6
Ian <ibrierley@gmail.com>
2015-09-30 11:24:16 +0100

    Update svgcanvas.js
    Changed docs and return value to match if unsuccessful

--- editor/svgcanvas.js
+++ editor/svgcanvas.js
@@ -4959,7 +4959,7 @@ this.setSvgString = function(xmlString) {
 // xmlString - The SVG as XML text.
 //
 // Returns:
-// This function returns false if the import was unsuccessful, true otherwise.
+// This function returns null if the import was unsuccessful, or the element otherwise.
 // TODO: 
 // * properly handle if namespace is introduced by imported content (must add to svgcontent
 // and update all prefixes in the imported node)
@@ -5080,7 +5080,7 @@ this.importSvgString = function(xmlString) {
 
 	} catch(e) {
 		console.log(e);
-		return false;
+		return null;
 	}
 
 	// we want to return the element so we can automatically select it
