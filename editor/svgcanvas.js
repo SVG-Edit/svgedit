c6255aaceb357aea5c79d1d006933767b47a738e
Jeff Schiller <codedread@gmail.com>
2015-12-03 00:24:28 -0800

    Update home page links to github

--- editor/svgcanvas.js
+++ editor/svgcanvas.js
@@ -111,7 +111,7 @@ var clearSvgContentElement = canvas.clearSvgContentElement = function() {
 	}).appendTo(svgroot);
 
 	// TODO: make this string optional and set by the client
-	var comment = svgdoc.createComment(" Created with SVG-edit - http://svg-edit.googlecode.com/ ");
+	var comment = svgdoc.createComment(" Created with SVG-edit - https://github.com/SVG-Edit/svgedit");
 	svgcontent.appendChild(comment);
 };
 clearSvgContentElement();
