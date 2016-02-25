0cf2fc57a15186ac6a829ac22c7cb9af5ec5928b
Magne Bratseth <magne.bratseth@weldindustry.com>
2016-02-25 09:03:20 +0100

    Slight changes to make the code more readable

--- editor/svgutils.js
+++ editor/svgutils.js
@@ -620,7 +620,6 @@ svgedit.utilities.assignAttributes = function(node, attrs, suspendLength, unitCh
 // Parameters:
 // element - DOM element to clean up
 svgedit.utilities.cleanupElement = function(element) {
-	var nodeName = element.nodeName ? element.nodeName : '';
 	var defaults = {
 		'fill-opacity':1,
 		'stop-opacity':1,
@@ -631,9 +630,16 @@ svgedit.utilities.cleanupElement = function(element) {
 		'stroke-linecap':'butt',
 		'stroke-opacity':1,
 		'stroke-width':1,
-		'rx': (nodeName == 'ellipse' ? null : 0),  // rx=0 and ry=0 is valid value which are different than null/empty
-		'ry': (nodeName == 'ellipse' ? null : 0)
+		'rx': 0,
+		'ry': 0
 	};
+
+	var nodeName = element.nodeName ? element.nodeName : '';
+	if (nodeName == 'ellipse') {
+		// Ellipse elements requires rx and ry attributes
+		delete defaults.rx;
+		delete defaults.ry;
+	}
 
 	var attr;
 	for (attr in defaults) {
