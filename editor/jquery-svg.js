4bfbaacb5ec535d4a7994953c8227ad80ee356e8
Brett Zamir <brettz9@yahoo.com>
2018-05-18 14:23:36 +0800

    - Complete conversion to 2-spaces, fixing issue #37 (also removed some extra/misplaced tabs/spaces in process)

--- editor/jquery-svg.js
+++ editor/jquery-svg.js
@@ -24,52 +24,52 @@
 // as an object with values for each given attributes
 
 var proxied = jQuery.fn.attr,
-	// TODO use NS.SVG instead
-	svgns = 'http://www.w3.org/2000/svg';
+  // TODO use NS.SVG instead
+  svgns = 'http://www.w3.org/2000/svg';
 jQuery.fn.attr = function (key, value) {
-	var i, attr;
-	var len = this.length;
-	if (!len) { return proxied.apply(this, arguments); }
-	for (i = 0; i < len; ++i) {
-		var elem = this[i];
-		// set/get SVG attribute
-		if (elem.namespaceURI === svgns) {
-			// Setting attribute
-			if (value !== undefined) {
-				elem.setAttribute(key, value);
-			} else if ($.isArray(key)) {
-				// Getting attributes from array
-				var j = key.length, obj = {};
+  var i, attr;
+  var len = this.length;
+  if (!len) { return proxied.apply(this, arguments); }
+  for (i = 0; i < len; ++i) {
+    var elem = this[i];
+    // set/get SVG attribute
+    if (elem.namespaceURI === svgns) {
+      // Setting attribute
+      if (value !== undefined) {
+        elem.setAttribute(key, value);
+      } else if ($.isArray(key)) {
+        // Getting attributes from array
+        var j = key.length, obj = {};
 
-				while (j--) {
-					var aname = key[j];
-					attr = elem.getAttribute(aname);
-					// This returns a number when appropriate
-					if (attr || attr === '0') {
-						attr = isNaN(attr) ? attr : (attr - 0);
-					}
-					obj[aname] = attr;
-				}
-				return obj;
-			}
-			if (typeof key === 'object') {
-				// Setting attributes form object
-				var v;
-				for (v in key) {
-					elem.setAttribute(v, key[v]);
-				}
-			// Getting attribute
-			} else {
-				attr = elem.getAttribute(key);
-				if (attr || attr === '0') {
-					attr = isNaN(attr) ? attr : (attr - 0);
-				}
-				return attr;
-			}
-		} else {
-			return proxied.apply(this, arguments);
-		}
-	}
-	return this;
+        while (j--) {
+          var aname = key[j];
+          attr = elem.getAttribute(aname);
+          // This returns a number when appropriate
+          if (attr || attr === '0') {
+            attr = isNaN(attr) ? attr : (attr - 0);
+          }
+          obj[aname] = attr;
+        }
+        return obj;
+      }
+      if (typeof key === 'object') {
+        // Setting attributes form object
+        var v;
+        for (v in key) {
+          elem.setAttribute(v, key[v]);
+        }
+      // Getting attribute
+      } else {
+        attr = elem.getAttribute(key);
+        if (attr || attr === '0') {
+          attr = isNaN(attr) ? attr : (attr - 0);
+        }
+        return attr;
+      }
+    } else {
+      return proxied.apply(this, arguments);
+    }
+  }
+  return this;
 };
 }());
--- firefox-extension/content/svg-edit-overlay.js
+++ firefox-extension/content/svg-edit-overlay.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-var */
 function startSvgEdit () { // eslint-disable-line no-unused-vars
-	var url = 'chrome://svg-edit/content/editor/svg-editor.html';
-	window.openDialog(url, 'SVG Editor', 'width=1024,height=700,menubar=no,toolbar=no');
+  var url = 'chrome://svg-edit/content/editor/svg-editor.html';
+  window.openDialog(url, 'SVG Editor', 'width=1024,height=700,menubar=no,toolbar=no');
 }
