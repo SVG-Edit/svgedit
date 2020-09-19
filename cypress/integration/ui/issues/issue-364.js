fa516488eb72ec987ca3306566dd244a43be4a09
jfh <dev@optimistik.fr>
2020-09-19 12:55:19 +0200

    added delay necessary to pass this test

--- cypress/integration/ui/issues/issue-364.js
+++ cypress/integration/ui/issues/issue-364.js
@@ -9,8 +9,10 @@ describe('Issue 364; IE errorwith rectangle selection by click', function () {
   });
 
   it('should set rectangle selection after click', function () {
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.get('#tools_rect_show')
       .trigger('mousedown', {force: true})
+      .wait(100) // this delay seems necessary
       .trigger('mouseup', {force: true})
       .should((button) => {
         expect(button).to.have.class('tool_button_current');
