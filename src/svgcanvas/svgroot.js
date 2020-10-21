7a1915bbe0fd0bf5ce96af62203d9a913d933c9f
Agriya Dev5 <agriya.dev5@agriya.in>
2020-10-21 20:08:19 +0530

    #refactor-canvas svg tag long string changes to es6 format

--- src/svgcanvas/svgroot.js
+++ src/svgcanvas/svgroot.js
@@ -18,20 +18,19 @@ import {text2xml} from '../common/utilities.js';
 export const svgRootElement = function (svgdoc, dimensions) {
   return svgdoc.importNode(
     text2xml(
-      '<svg id="svgroot" xmlns="' + NS.SVG + '" xlinkns="' + NS.XLINK + '" ' +
-    'width="' + dimensions[0] + '" height="' + dimensions[1] +
-        '" x="' + dimensions[0] + '" y="' + dimensions[1] + '" overflow="visible">' +
-    '<defs>' +
-        '<filter id="canvashadow" filterUnits="objectBoundingBox">' +
-        '<feGaussianBlur in="SourceAlpha" stdDeviation="4" result="blur"/>' +
-        '<feOffset in="blur" dx="5" dy="5" result="offsetBlur"/>' +
-        '<feMerge>' +
-            '<feMergeNode in="offsetBlur"/>' +
-            '<feMergeNode in="SourceGraphic"/>' +
-        '</feMerge>' +
-        '</filter>' +
-    '</defs>' +
-    '</svg>'
+      `<svg id="svgroot" xmlns="${NS.SVG}" xlinkns="${NS.XLINK}" width="${dimensions[0]}" 
+        height="${dimensions[1]}" x="${dimensions[0]}" y="${dimensions[1]}" overflow="visible">
+        <defs>
+          <filter id="canvashadow" filterUnits="objectBoundingBox">
+            <feGaussianBlur in="SourceAlpha" stdDeviation="4" result="blur"/>
+            <feOffset in="blur" dx="5" dy="5" result="offsetBlur"/>
+            <feMerge>
+              <feMergeNode in="offsetBlur"/>
+              <feMergeNode in="SourceGraphic"/>
+            </feMerge>
+          </filter>
+        </defs>
+      </svg>`
     ).documentElement,
     true
   );
