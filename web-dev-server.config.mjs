74837430f42eebeae540a4a112f5d3e61bc3d5d9
JFH <20402845+jfhenon@users.noreply.github.com>
2020-12-19 23:12:04 +0100

    Update web-dev-server.config.mjs

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,5 +1,6 @@
-import {fromRollup} from "@web/dev-server-rollup";
-import rollupCommonjs from "@rollup/plugin-commonjs";
+import {fromRollup} from '@web/dev-server-rollup';
+// eslint-disable-next-line node/no-unpublished-import
+import rollupCommonjs from '@rollup/plugin-commonjs';
 
 const commonjs = fromRollup(rollupCommonjs);
 
