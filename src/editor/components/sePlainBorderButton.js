f713ba04d9b328d365b67c440cecea9ea6cd463b
Agriya Dev5 <agriya.dev5@agriya.in>
2021-01-19 16:12:23 +0530

    #69 menu button style overwrite

---
+++ src/editor/components/sePlainBorderButton.js
@@ -0,0 +1,34 @@
+/* eslint-disable node/no-unpublished-import */
+import {template} from 'elix/src/base/internal.js';
+import {fragmentFrom} from 'elix/src/core/htmlLiterals.js';
+import PlainButton from 'elix/src/plain/PlainButton.js';
+
+/**
+ * @class SePlainBorderButton
+ * Button with a border in the Plain reference design system
+ *
+ * @inherits PlainButton
+ */
+class SePlainBorderButton extends PlainButton {
+  /**
+    * @function get
+    * @returns {PlainObject}
+  */
+  get [template] () {
+    const result = super[template];
+    result.content.append(
+      fragmentFrom.html`
+        <style>
+          [part~="button"] {
+            background: #72797A;
+            border: 1px solid #ccc;
+            padding: 0.25em 0.5em;
+          }
+        </style>
+      `
+    );
+    return result;
+  }
+}
+
+export default SePlainBorderButton;
