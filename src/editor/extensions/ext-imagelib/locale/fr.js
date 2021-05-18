bbffc42cca9fddb53de4a80b19740d5a07f15ad9
Agriya Dev5 <agriya.dev5@agriya.in>
2021-05-18 19:18:02 +0530

    #98 extension imagelib i18n modification

--- src/editor/extensions/ext-imagelib/locale/fr.js
+++ src/editor/extensions/ext-imagelib/locale/fr.js
@@ -9,25 +9,8 @@ export default {
       title: "Bibliothèque d'images"
     }
   ],
-  imgLibs: [
-    {
-      name: 'Demo library (local)',
-      url: 'extensions/ext-imagelib/index.html',
-      description: 'Demonstration library for SVG-edit on this server'
-    },
-    {
-      name: 'IAN Symbol Libraries',
-      url: 'https://ian.umces.edu/symbols/catalog/svgedit/album_chooser.php?svgedit=3',
-      description: 'Free library of illustrations'
-    }
-    /*
-    // See message in "en" locale for further details
-    ,
-    {
-      name: 'Openclipart',
-      url: 'https://openclipart.org/svgedit',
-      description: 'Share and Use Images. Over 100,000 Public Domain SVG Images and Growing.'
-    }
-    */
-  ]
+  imgLibs_0_name: 'Demo library (local)',
+  imgLibs_0_description: 'Demonstration library for SVG-edit on this server',
+  imgLibs_1_name: 'IAN Symbol Libraries',
+  imgLibs_1_description: 'Free library of illustrations',
 };
