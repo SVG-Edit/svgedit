1cc489a3fca5d8fe540cb2301662506fc4225b25
JFH <20402845+jfhenon@users.noreply.github.com>
2021-07-30 11:43:26 +0200

    Update ext-opensave.js

--- src/editor/extensions/ext-opensave/ext-opensave.js
+++ src/editor/extensions/ext-opensave/ext-opensave.js
@@ -20,58 +20,90 @@ export default {
   name: 'opensave',
   init ({ encode64 }) {
     const svgEditor = this;
+    // check suppport for NativeFileAPI
+    const supportsNativeFileApi = !!window.showOpenFilePicker;
+    let fileHandle;
 
     svgEditor.setCustomHandlers({
-      save (win, svg) {
+      async save (win, svg) {
         this.showSaveWarning = false;
 
         // by default, we add the XML prolog back, systems integrating SVG-edit (wikis, CMSs)
         // can just provide their own custom save handler and might not want the XML prolog
         svg = '<?xml version="1.0"?>\n' + svg;
 
-        // Since saving SVGs by opening a new window was removed in Chrome use artificial link-click
+        if (fileHandle) {
+          const writableStream = await fileHandle.createWritable();
+          writableStream.write(svg);
+          writableStream.close();
+        } else {
+          // legacy save
+          // Since saving SVGs by opening a new window was removed in Chrome use artificial link-click
         // https://stackoverflow.com/questions/45603201/window-is-not-allowed-to-navigate-top-frame-navigations-to-data-urls
-        const a = document.createElement('a');
-        a.href = 'data:image/svg+xml;base64,' + encode64(svg);
-        a.download = 'icon.svg';
-        a.style.display = 'none';
-        document.body.append(a); // Need to append for Firefox
+          const a = document.createElement('a');
+          a.href = 'data:image/svg+xml;base64,' + encode64(svg);
+          a.download = 'icon.svg';
+          a.style.display = 'none';
+          document.body.append(a); // Need to append for Firefox
 
-        a.click();
+          a.click();
 
-        // Alert will only appear the first time saved OR the
-        //   first time the bug is encountered
-        const done = this.configObj.pref('save_notice_done');
+          // Alert will only appear the first time saved OR the
+          //   first time the bug is encountered
+          const done = this.configObj.pref('save_notice_done');
 
-        if (done !== 'all') {
-          const note = svgEditor.i18next.t('notification.saveFromBrowser', { type: 'SVG' });
+          if (done !== 'all') {
+            const note = svgEditor.i18next.t('notification.saveFromBrowser', { type: 'SVG' });
 
-          this.configObj.pref('save_notice_done', 'all');
-          if (done !== 'part') {
-            seAlert(note);
+            this.configObj.pref('save_notice_done', 'all');
+            if (done !== 'part') {
+              seAlert(note);
+            }
           }
+
         }
+
       },
       async open () {
-        const ok = await this.openPrep();
-        if (ok === 'Cancel') { return; }
+        // ask user before clearing an unsaved SVG
+        const response = await this.openPrep();
+        if (response === 'Cancel') { return; }
         this.svgCanvas.clear();
-        const input = document.createElement('input');
-        input.type = 'file';
-        input.addEventListener('change', (e) => {
-          // getting a hold of the file reference
-          const file = e.target.files[0];
-          // setting up the reader
-          const reader = new FileReader();
-          reader.readAsText(file, 'UTF-8');
-          // here we tell the reader what to do when it's done reading...
-          reader.addEventListener('load', async (readerEvent) => {
-            const content = readerEvent.target.result;
-            await this.loadSvgString(content);
-            this.updateCanvas();
+        if (supportsNativeFileApi) {
+          const options = {
+            multiple: false,
+            types: [
+              {
+                accept: {
+                  'image/svg+xml': '.svg'
+                }
+              }
+            ],
+            excludeAcceptAllOption: true
+          };
+          [ fileHandle ] = await window.showOpenFilePicker(options);
+          const file = await fileHandle.getFile();
+          const svgContent = await file.text();
+          await this.loadSvgString(svgContent);
+          this.updateCanvas();
+        } else {
+          const input = document.createElement('input');
+          input.type = 'file';
+          input.addEventListener('change', (e) => {
+            // getting a hold of the file reference
+            const file = e.target.files[0];
+            // setting up the reader
+            const reader = new FileReader();
+            reader.readAsText(file, 'UTF-8');
+            // here we tell the reader what to do when it's done reading...
+            reader.addEventListener('load', async (readerEvent) => {
+              const content = readerEvent.target.result;
+              await this.loadSvgString(content);
+              this.updateCanvas();
+            });
           });
-        });
-        input.click();
+          input.click();
+        }
       }
     });
   }
