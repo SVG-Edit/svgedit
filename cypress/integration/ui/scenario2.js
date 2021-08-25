ae4610edfb5341c028f3f737e08041bc9eef6e44
agriyadev5 <agriya.dev5@agriya.in>
2021-08-25 16:24:38 +0530

    #623 circle, fhellipse and ellipse cypress test case added

--- cypress/integration/ui/scenario2.js
+++ cypress/integration/ui/scenario2.js
@@ -23,20 +23,12 @@ describe('use all parts of svg-edit', function () {
     cy.get('#tool_source_save').click({ force: true });
     testSnapshot();
   });
-  it('check tool_path', function () {
-    cy.get('#tool_path')
+  it('check tool_circle', function () {
+    cy.get('#tool_circle')
       .click({ force: true });
     cy.get('#svgcontent')
-      .trigger('mousedown', 50, 50, { force: true })
-      .trigger('mouseup', { force: true })
-      .trigger('mousemove', 100, 50, { force: true })
-      .trigger('mousedown', 100, 50, { force: true })
-      .trigger('mouseup', { force: true })
-      .trigger('mousemove', 75, 150, { force: true })
-      .trigger('mousedown', 75, 150, { force: true })
-      .trigger('mouseup', { force: true })
-      .trigger('mousemove', 0, 0, { force: true })
-      .trigger('mousedown', 0, 0, { force: true })
+      .trigger('mousedown', 150, 150, { force: true })
+      .trigger('mousemove', 250, 200, { force: true })
       .trigger('mouseup', { force: true });
     cy.get('#svgcontent').toMatchSnapshot();
   });
@@ -52,16 +44,11 @@ describe('use all parts of svg-edit', function () {
       .trigger('mouseup', 200, 80, { force: true });
     cy.get('#svgcontent').toMatchSnapshot();
   });
-  it('check tool_ellipse_circle', function () {
+  it('check tool_ellipse', function () {
     cy.get('#tool_ellipse').click({ force: true });
     cy.get('#svgcontent')
       .trigger('mousedown', 75, 150, { force: true })
       .trigger('mousemove', 130, 175, { force: true })
-      .trigger('mouseup', { force: true });
-    cy.get('#tool_circle').click({ force: true });
-    cy.get('#svgcontent')
-      .trigger('mousedown', 150, 50, { force: true })
-      .trigger('mousemove', 150, 80, { force: true })
       .trigger('mouseup', { force: true });
     cy.get('#svgcontent').toMatchSnapshot();
   });
@@ -73,7 +60,6 @@ describe('use all parts of svg-edit', function () {
   });
   it('check tool_circle_change_opacity', function () {
     cy.get('#svg_2').click({ force: true });
-    // cy.get('#opacity').trigger('click', 'bottomRight');
     for(let n = 0; n < 10; n ++){
       cy.get('#opacity').shadow().find('elix-number-spin-box').eq(0).shadow().find('#downButton').eq(0)
         .click({ force: true });
@@ -82,12 +68,57 @@ describe('use all parts of svg-edit', function () {
   });
   it('check tool_ellipse_change_rotation', function () {
     cy.get('#svg_3').click({ force: true });
-    // cy.get('#opacity').trigger('click', 'bottomRight');
     for(let n = 0; n < 5; n ++){
       cy.get('#angle').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
         .click({ force: true });
     }
     cy.get('#svgcontent').toMatchSnapshot();
   });
-
+  it('check tool_ellipse_change_blur', function () {
+    cy.get('#svg_3').click({ force: true });
+    for(let n = 0; n < 10; n ++){
+      cy.get('#blur').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
+        .click({ force: true });
+    }
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_ellipse_change_cx_cy_coordinate', function () {
+    cy.get('#svg_3').click({ force: true });
+    for(let n = 0; n < 25; n ++){
+      cy.get('#ellipse_cx').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
+        .click({ force: true });
+    }
+    for(let n = 0; n < 25; n ++){
+      cy.get('#ellipse_cy').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
+        .click({ force: true });
+    }
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_ellipse_change_rx_ry_radius', function () {
+    cy.get('#svg_3').click({ force: true });
+    for(let n = 0; n < 25; n ++){
+      cy.get('#ellipse_rx').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
+        .click({ force: true });
+    }
+    for(let n = 0; n < 25; n ++){
+      cy.get('#ellipse_ry').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
+        .click({ force: true });
+    }
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_ellipse_bring_to_back', function () {
+    cy.get('#svg_2').click({ force: true });
+    cy.get('#tool_move_bottom').click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_ellipse_bring_to_front', function () {
+    cy.get('#svg_2').click({ force: true });
+    cy.get('#tool_move_top').click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_ellipse_clone', function () {
+    cy.get('#svg_2').click({ force: true });
+    cy.get('#tool_clone').click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
 });
