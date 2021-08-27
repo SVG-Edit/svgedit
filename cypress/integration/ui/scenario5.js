92ec863fbc535d2665e6268baf6509b28974262c
agriyadev5 <agriya.dev5@agriya.in>
2021-08-27 14:11:11 +0530

    #623 line missing test case added

--- cypress/integration/ui/scenario5.js
+++ cypress/integration/ui/scenario5.js
@@ -94,7 +94,17 @@ describe('use all parts of svg-edit', function () {
     cy.get('#tool_clone').click({ force: true });
     cy.get('#svgcontent').toMatchSnapshot();
   });
-  it('check tool_fhrect_change_x_y_coordinate', function () {
+  it('check tool_line_bring_to_back', function () {
+    cy.get('#svg_2').click({ force: true });
+    cy.get('#tool_move_bottom').click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_line_bring_to_front', function () {
+    cy.get('#svg_2').click({ force: true });
+    cy.get('#tool_move_top').click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_line_change_x_y_coordinate', function () {
     cy.get('#svg_2').click({ force: true });
     for(let n = 0; n < 25; n ++){
       cy.get('#line_x1').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
@@ -112,6 +122,46 @@ describe('use all parts of svg-edit', function () {
       cy.get('#line_y2').shadow().find('elix-number-spin-box').eq(0).shadow().find('#downButton').eq(0)
         .click({ force: true });
     }
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_line_change_stroke_width', function () {
+    cy.get('#svg_2').click({ force: true });
+    for(let n = 0; n < 10; n ++){
+      cy.get('#stroke_width').shadow().find('elix-number-spin-box').eq(0).shadow().find('#upButton').eq(0)
+        .click({ force: true });
+    }
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_line_change_stroke_style', function () {
+    cy.get('#svg_2').click({ force: true });
+    cy.get('#stroke_style').shadow().find('elix-dropdown-list').eq(0).invoke('attr', 'opened', 'opened');
+    cy.get('#stroke_style').find('se-list-item').eq(3).shadow().find('elix-option').eq(0)
+      .click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_line_change_line_cap', function () {
+    cy.get('#svg_2').click({ force: true });
+    cy.get('#stroke_linecap').shadow().find('elix-dropdown-list').eq(0).invoke('attr', 'opened', 'opened');
+    cy.get('#stroke_linecap').find('se-list-item').eq(1).shadow().find('elix-option').eq(0)
+      .click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_line_align_to_page', function () {
+    cy.get('#svg_3').click({ force: true });
+    cy.get('#tool_position').shadow().find('elix-dropdown-list').eq(0).invoke('attr', 'opened', 'opened');
+    cy.get('#tool_position').find('se-list-item').eq(2).shadow().find('elix-option').eq(0)
+      .click({ force: true });
+    cy.get('#svgcontent').toMatchSnapshot();
+  });
+  it('check tool_line_change_stoke_color', function () {
+    cy.get('#svg_3').click({ force: true });
+    cy.get('#stroke_color').shadow().find('#picker').eq(0).click({ force: true });
+    cy.get('#stroke_color').shadow().find('#color_picker').eq(0)
+      .find('#jGraduate_colPick').eq(0).find('#jPicker-table').eq(0)
+      .find('.QuickColor').eq(9).click({ force: true });
+    cy.get('#stroke_color').shadow().find('#color_picker').eq(0)
+      .find('#jGraduate_colPick').eq(0).find('#jPicker-table').eq(0)
+      .find('#Ok').eq(0).click({ force: true });
     cy.get('#svgcontent').toMatchSnapshot();
   });
 });
