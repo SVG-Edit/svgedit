fcfe6c5a6e3977312d0058b970f5841f3265e5c3
JFH <20402845+jfhenon@users.noreply.github.com>
2021-09-05 11:01:59 +0200

    fix test

--- src/editor/ConfigObj.js
+++ src/editor/ConfigObj.js
@@ -122,7 +122,7 @@ export default class ConfigObj {
       text: {
         stroke_width: 0,
         font_size: 24,
-        font_family: 'serif'
+        font_family: 'Serif'
       },
       initOpacity: 1,
       initTool: 'select',
