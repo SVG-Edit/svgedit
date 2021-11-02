61ce05599615d1287ec1a6413e264f75326ac24b
Simon Brown <simon@magnetize.co.uk>
2021-11-02 15:42:18 +0000

    Reproduction of Chrome 96 text scale bug #660

---
+++ cypress/integration/ui/issues/issue-660.js
@@ -0,0 +1,49 @@
+import {
+  visitAndApproveStorage
+} from '../../../support/ui-test-helper.js';
+
+// See https://github.com/SVG-Edit/svgedit/issues/660
+describe('Fix issue 660', function () {
+  beforeEach(() => {
+    visitAndApproveStorage();
+  });
+
+  it('can resize text', function () {
+    cy.get('#tool_source').click();
+    cy.get('#svg_source_textarea')
+      .type('{selectall}', { force: true })
+      .type(`<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg">
+      <g class="layer">
+       <title>Layer 1</title>
+       <text fill="#000000" id="a_text" text-anchor="middle" x="260.5" xml:space="preserve" y="192.5" font-size="40">hello</text>
+      </g>
+     </svg>`, { force: true, parseSpecialCharSequences: false });
+    cy.get('#tool_source_save').click({ force: true });
+    cy.get('#a_text').should('exist');
+    cy.get('#a_text')
+      .trigger('mousedown', { which: 1, force: true })
+      .trigger('mouseup', { force: true });
+    cy.get('#selectorGrip_resize_s')
+      .trigger('mousedown', { which: 1, force: true })
+
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      .trigger('mousemove', 0, 20, { force: true })
+      
+      // // .trigger('mousemove', 0, 100, { force: true }) // did not produce it
+      // // .trigger('mousemove', 0, 100, { force: true })
+
+      // // .trigger('mousemove', 0, 200, { force: true }) // did not produce it
+      .trigger('mouseup', { force: true });
+    // svgedit use the #text text field to capture the text
+    cy.get('#a_text').should('have.attr', 'transform')
+      .and('equal', 'matrix(1 0 0 4.54167 0 -540.104)'); // 10x20
+  });
+});
