62d5e0776316d5c792d53b638097dc2897e02dc1
zktosu <zktosu@gmail.com>
2022-04-30 22:53:06 -0400

    Create tr.js

---
+++ src/editor/extensions/ext-polystar/locale/tr.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'yıldız',
+  title: 'Çokgen/Yıldız Aracı',
+  buttons: [
+    {
+      title: 'Yıldız Aracı'
+    },
+    {
+      title: 'Çokgen Aracı'
+    }
+  ],
+  contextTools: [
+    {
+      title: 'Kenar Sayısı',
+      label: 'noktalar'
+    },
+    {
+      title: 'Sivrilik',
+      label: 'Sivrilik'
+    },
+    {
+      title: 'Yıldızı Kıvır',
+      label: 'Döngüsel Kaydırma'
+    },
+    {
+      title: 'Kenar Sayısı',
+      label: 'kenarlar'
+    }
+  ]
+}
