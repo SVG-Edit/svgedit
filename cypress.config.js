9e3a4a90912481857703060a79093eb97e106cc1
JFH <20402845+jfhenon@users.noreply.github.com>
2022-06-18 10:32:27 +0200

    cypress 10 migration (#777)
    * cypress 10 migration
    
    * fix lint

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  video: false,
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 120000,
+  includeShadowDom: true,
+  scrollBehavior: false,
+  'cypress-plugin-snapshots': {
+    autoCleanUp: true,
+    prettier: true,
+    updateSnapshots: false,
+    diffLines: 5
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents (on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8000',
+    excludeSpecPattern: ['**/__snapshots__/*', '**/__image_snapshots__/*']
+  }
+})
