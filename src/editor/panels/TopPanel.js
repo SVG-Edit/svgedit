0f3d25d978fd6ed82dab987dd47394c9814fd0b3
pmkrawczyk <63370626+pmkrawczyk@users.noreply.github.com>
2022-07-23 23:05:31 +0200

    Update TopPanel.js
    Enable text tool panel wen multiple text elements are selected.

--- src/editor/panels/TopPanel.js
+++ src/editor/panels/TopPanel.js
@@ -422,6 +422,22 @@ class TopPanel {
 
       // if (elem)
     } else if (this.multiselected) {
+      // Check if all selected elements have the same tag
+      const selElems = this.editor.svgCanvas.getSelectedElements()
+      const tagNames = []
+      selElems.forEach(elem => {
+        const { tagName } = elem
+        tagNames.push(tagName)
+      })
+      const allElemsSameTag = tagNames.every((val, i, arr) => val === arr[0])
+
+      // Enable panels specific for selected elements
+      if (allElemsSameTag) {
+        if (tagNames[0] === 'text') {
+          this.displayTool('text_panel')
+        }
+      }
+      
       this.displayTool('multiselected_panel')
       menuItems.setAttribute('enablemenuitems', '#group')
       menuItems.setAttribute('disablemenuitems', '#ungroup')
