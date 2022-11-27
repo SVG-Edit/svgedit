993d851dbb9b365a7676dd47b85b31471d9a5aa3
JFH <20402845+jfhenon@users.noreply.github.com>
2022-11-27 22:28:02 +0100

    publish svgcanvas

--- cypress/replace-in-file.config.js
+++
@@ -1,7 +0,0 @@
-const options = {
-  files: 'instrumented/**',
-  from: "import SvgCanvas from '@svgedit/svgcanvas';",
-  to: "import SvgCanvas from '@svgedit/svgcanvas/instrumented/svgcanvas.js';"
-}
-
-module.exports = options
