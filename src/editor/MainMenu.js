6eafa753258edaf89d481a5cd8065c67cb1b3063
Paul Korecky <113685431+spaghetti22@users.noreply.github.com>
2024-05-26 14:09:22 +0200

    fix double binding of shortcut "d" (#972)

--- src/editor/MainMenu.js
+++ src/editor/MainMenu.js
@@ -228,7 +228,7 @@ class MainMenu {
     template.innerHTML = `
     <se-menu id="main_button" label="SVG-Edit" src="logo.svg" alt="logo">
         <se-menu-item id="tool_export" label="tools.export_img" src="export.svg"></se-menu-item>
-        <se-menu-item id="tool_docprops" label="tools.docprops" shortcut="D" src="docprop.svg"></se-menu-item>
+        <se-menu-item id="tool_docprops" label="tools.docprops" shortcut="shift+D" src="docprop.svg"></se-menu-item>
         <se-menu-item id="tool_editor_prefs" label="config.editor_prefs" src="editPref.svg"></se-menu-item>
         <se-menu-item id="tool_editor_homepage" label="tools.editor_homepage" src="logo.svg"></se-menu-item>
     </se-menu>`
