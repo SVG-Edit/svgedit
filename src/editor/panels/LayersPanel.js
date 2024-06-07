f0fa50f289f02e7ed6b37a93c311ee9b0b9b791d
w8w8w8 <asato@live.com>
2024-06-07 23:02:37 +0800

    Update LayersPanel.js
    fix the eye icon lost at right layers

--- src/editor/panels/LayersPanel.js
+++ src/editor/panels/LayersPanel.js
@@ -256,6 +256,14 @@ class LayersPanel {
       layerTr.className = (name === currentLayerName) ? 'layer layersel' : 'layer'
       const layerVis = document.createElement('td')
       layerVis.className = (!drawing.getLayerVisibility(name)) ? 'layerinvis layervis' : 'layervis'
+
+      // fix the eye icon lost at right layers
+      const _eye = document.createElement('img');
+      _eye.src="./images/eye.svg";
+      _eye.style.width = '14px';
+      _eye.style.width = '14px';
+      layerVis.appendChild(_eye);
+      
       const layerName = document.createElement('td')
       layerName.className = 'layername'
       layerName.textContent = name
