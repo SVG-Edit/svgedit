3843c1f95b57600d67022eac1ac9e589a352d2db
JFH <20402845+jfhenon@users.noreply.github.com>
2024-08-16 16:21:44 +0200

    fix test

--- cypress/support/ui-test-helper.js
+++ cypress/support/ui-test-helper.js
@@ -1,7 +1,7 @@
 export const visitAndApproveStorage = () => {
   cy.clearLocalStorage()
   cy.clearCookies()
-  cy.visit('/instrumented/editor/index.html')
+  cy.visit('/src/editor/index.html')
   cy.get('#storage_ok').click({ force: true })
   // move to English and snap mode (to correct potential differences between CI and local tests )
   selectEnglishAndSnap()
